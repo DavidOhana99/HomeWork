7b2b96c3591f9fc015b81a9c1a6c82938585e478
DavidOhana99 <davidohana1999@gmail.com>
2024-05-23 19:41:02 -0400

    "C:\xampp\htdocs\Class\html\51\HW 51.cs"

---
+++ html/51/HW 51.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    internal class HW_51
+    {
+        static void Main(string[] args)
+        {
+            shirt();
+        }
+
+        static void shirt()
+        {
+            string[] colors = { "red", "green", "blue" };
+            string[] patterns = { "striped", "checked", "plain" };
+
+
+            
+
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+
+                for (int j = 0; j < patterns.Length; j++)
+                {
+
+                    Console.WriteLine(colors[i] + " : " + patterns[j]);
+                }
+            }
+
+        }
+
+       
+    }
+}
