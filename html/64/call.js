6b35d6d57283044ae49b070457540e1f0d178d6c
DavidOhana99 <davidohana1999@gmail.com>
2024-07-15 20:21:27 -0400

    js

---
+++ html/64/call.js
@@ -0,0 +1,7 @@
+'use strict';
+console.log(name);
+console.log(email);
+food = prompt("Whats your favorite food?");
+alert(food);
+Name = prompt("whats ur name?");
+confirm(Name);
---
+++ html/64/index.js
@@ -0,0 +1,3 @@
+'use strict';
+const name = "Davidohana";
+const email = "davidohana1999@gmail.com";
