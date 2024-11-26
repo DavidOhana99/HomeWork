5a47bcb464c51a6fc03452f0906ef60129569d0d
DavidOhana99 <davidohana1999@gmail.com>
2024-11-26 16:52:37 -0500

    weather

---
+++ html/89/vite-project/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  const [count, setCount] = useState(0)
+  const [weatherData, setWeatherData] = useState({})
+  //make a state like above called weatherData, setWeatherData
+  const apiKey = '4d940566413cbb48ddbe156f2b502364'
+  const icon = `https://openweathermap.org/img/w`
+  // const icon =   `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`
+  // 
+  // const getWeather = async () => 
+  async function getweatherdata() {
+    try {
+      const r = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=08701,US&appid=${apiKey}&units=imperial&lang=he`);
+
+      const data = await r.json();
+
+      setWeatherData(data)
+
+
+    } catch (e) {
+      console.error('oops', e);
+    }
+  }
+
+
+  useEffect(() => {
+    getweatherdata()
+  }, []);
+
+  //make a useEffect
+  //inside get weather response
+  //const response = await  getWeather()
+  // setWeatherData({location: lakeood, temp: 78})
+  console.log({ weatherData });
+  if (!weatherData?.name) return <>loading</>
+
+  return (
+    <>
+
+      <div className="card">
+          weather in {weatherData.name} is {weatherData.main.temp}
+        <img
+          src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+        />
+      </div>
+
+    </>
+  )
+}
+
+export default App
