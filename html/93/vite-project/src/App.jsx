5d99ed4bda02a1398861a290231d305f1cabc917
DavidOhana99 <davidohana1999@gmail.com>
2024-12-12 21:25:09 -0500

    done

--- html/93/vite-project/src/App.jsx
+++ html/93/vite-project/src/App.jsx
@@ -6,15 +6,29 @@ import './App.css'
 const App = () => {
   const [bgColor, setBgColor] = useState('#ffffff'); // Default background color
   const [textColor, setTextColor] = useState('#000000'); // Default text color
+  const [font, setFont] = useState('Arial');
 
-  return (
+  const handleFontChange = (event) => {
+    setFont(event.target.value); // Update the font state
+  };
+
+    return (
     <div style={{
       color: textColor, backgroundColor: bgColor,
       height: 679,
       width:1525,
       transition: 'background-color 0.3s, color 0.3s',
-      textAlign:'center'
-    }}>
+      textAlign:'center',
+      fontFamily: font
+      }}>
+         <h1>Font Chooser</h1>
+      <select onChange={handleFontChange} value={font}>
+        <option value="Arial">Arial</option>
+        <option value="Courier New">Courier New</option>
+        <option value="Georgia">Georgia</option>
+        <option value="Times New Roman">Times New Roman</option>
+        <option value="Verdana">Verdana</option>
+      </select>
       <h1 style={{margin:0}}>Color Picker App</h1>
       <label>
         Background Color:
