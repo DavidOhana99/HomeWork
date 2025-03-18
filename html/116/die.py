825fe38551d62db8b87cc4afcac9d41d276b9eb8
DavidOhana99 <davidohana1999@gmail.com>
2025-03-18 19:59:55 -0400

    n

--- html/116/die.py
+++ html/116/die.py
@@ -1,11 +1,13 @@
 import random
 
+
 class Die:
     def __init__(self, sides=6):
         self.sides = sides
-    
+
     def roll(self):
         return random.randint(1, self.sides)
+
 
 my_die = Die()
 
